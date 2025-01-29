68b29f8ea06eb682d2762aa57ccdc77702185bce
AgathaBahati <matinodev@gmail.com>
2025-01-29 21:54:37 +0300

    feat: setup config settings and dependencies

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+    db_host: str
+    db_port: str
+    
+    model_config = SettingsConfigDict(env_file="/.env")
