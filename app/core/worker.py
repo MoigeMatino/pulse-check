094a58689b04589eeb1615789e59cc6ca6832fc1
AgathaBahati <matinodev@gmail.com>
2025-03-22 23:58:29 +0300

    feat: add periodic ssl status check task

--- app/core/worker.py
+++ app/core/worker.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 import os
 
 BROKER_URL = os.getenv("CELERY_BROKER_URL")
@@ -18,3 +19,14 @@ celery_app.conf.update(
     timezone="UTC",
     result_expires=3600,
 )
+
+celery_app.conf.beat_schedule = {
+    # SSL Check Task (runs daily at midnight)
+    "periodic-ssl-check": {
+        "task": "app.tasks.ssl_checker.periodic_ssl_check",  
+        "schedule": crontab(hour=0, minute=0),  # Run daily at midnight
+    },
+}
+
+# Configure directory path to celerybeat-schedule file(file used by Celery Beat to store the state of periodic tasks)
+celery_app.conf.beat_schedule_filename = "/var/run/celery/celerybeat-schedule"
