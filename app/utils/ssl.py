812a097b4373b2cc10b68aedb06f8b83a512f4cc
AgathaBahati <matinodev@gmail.com>
2025-03-24 21:53:23 +0300

    feat: add is_valid filter to get ssl logs endpoint

--- app/utils/ssl.py
+++ app/utils/ssl.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import SQLAlchemyError
+from fastapi import Query
 from sqlmodel import Session, select
 from app.api.v1.models import SSLLog
 
@@ -10,3 +10,11 @@ def get_ssl_logs_by_website_id(db: Session, website_id: str):
     ssl_logs = db.exec(statement).all()
 
     return ssl_logs
+
+def valid_logs_query(query: Query, is_valid: bool) -> Query:
+    """Apply validity filter to  query if specified"""  
+    return query.where(SSLLog.is_valid == is_valid)
+
+def all_logs_query(db: Session, website_id: str) -> Query:
+    """Create a query to retrieve SSL logs for a specific website"""
+    return select(SSLLog).where(SSLLog.website_id == website_id)
